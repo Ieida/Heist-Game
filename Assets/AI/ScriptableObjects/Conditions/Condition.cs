f12f4ada768b97f669051b08a4e30c0d76e23166
gomnosano@gmail.com <gomnosano@gmail.com>
2023-05-06 11:54:34 +0200

    Condition.Evaluate() now correctly returns an int

--- Assets/AI/ScriptableObjects/Conditions/Condition.cs
+++ Assets/AI/ScriptableObjects/Conditions/Condition.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Condition", menuName = "Conditions/Condition", order = 0)]
 public class Condition : ScriptableObject
 {
-  public abstract void Evaluate();
+  public abstract int Evaluate();
 }
