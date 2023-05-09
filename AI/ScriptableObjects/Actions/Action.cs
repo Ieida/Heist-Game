5d5285c9687c2cb57f2e6741158cfae1f7f96ce2
Ieida <37672966+Ieida@users.noreply.github.com>
2023-05-09 08:57:30 +0000

    Deleting before remaking

--- AI/ScriptableObjects/Actions/Action.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Action", menuName = "Actions/Action", order = 0)]
-public class Action : ScriptableObject
-{
-  public abstract void Update();
-}
--- AI/ScriptableObjects/Conditions/Condition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Condition", menuName = "Conditions/Condition", order = 0)]
-public class Condition : ScriptableObject
-{
-  [field:SerializeField] int Points{get;set;}
-  
-  public abstract bool Evaluate();
-}
